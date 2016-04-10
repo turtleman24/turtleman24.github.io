246022f645d2a8e182be9ef0442698d249c12b42
turtleman24 <rytse11ers@gmail.com>
2016-04-10 17:43:16 -0600

    picture switch

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-function swithpiture() {
-    var tag=document.getElementById ("switch");
-    tag.style.backgroundImage="0_CATABA_Turtle_p.jpg"
+function switchpicture() {
+    var tag=document.getElementById("switch");
+    tag.style.backgroundImage="url('0_CATAWBA_Turtle_p.jpg')"
+    console.log(tag.style.backgroundImage);
+    alert('See, it\'s easy!.  Good job!');
 }
