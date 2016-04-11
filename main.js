853492a58e7913d4c2c0f4960bf307a0237e409b
turtleman24 <rytse11ers@gmail.com>
2016-04-10 20:09:33 -0600

    endless

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
+var selectedImage=0;
 function switchpicture() {
+    var images = ["url(http://ultimatewalrus.com/turtles/img2/o-TURTLE-facebook.jpg)",
+                  "url('0_CATAWBA_Turtle_p.jpg')"];
     var tag=document.getElementById("switch");
-    tag.style.backgroundImage="url('0_CATAWBA_Turtle_p.jpg')"
-    console.log(tag.style.backgroundImage);
-    alert('See, it\'s easy!.  Good job!');
+    selectedImage = (selectedImage+1) % 2;
+    tag.style.backgroundImage=images[selectedImage];
+    if (selectedImage==1) {
+        alert('See, it\'s easy!.  Good job!');
+    }
 }
